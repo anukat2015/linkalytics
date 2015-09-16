56dea22954b03fa8e69c4b9f46d2102ee26da48b
mattgroh <matthew.groh@gmail.com>
2015-09-16 18:04:47 -0400

    Updating test

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def main():
     q2 = rq.Queue('youtube', connection=redis.Redis())
     job1 = q1.enqueue(twitter.run, {"text": "twitter.com/realDonaldTrump"})
     job2 = q1.enqueue(twitter.run, {"text": "Here's Nassim Taleb's twitter.....@nntaleb"})
-    job3 = q2.enqueue(youtube.run, {"text": "Have you seen this https://www.youtube.com/watch?t=3&v=ToyoBTiwZ6c youtube video with Super Mario"})
+    job3 = q2.enqueue(youtube.run, {"text": "Have you seen this https://www.youtube.com/watch?t=3&v=ToyoBTiwZ6c youtube video with Super Marioll"})
 
     while True:
         time.sleep(1)
