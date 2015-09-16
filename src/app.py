66ddf55c7b0563fe390f4a4685d9fa02d0642718
Eric Chu <eytchu@gmail.com>
2015-09-15 22:01:51 -0700

    MEM-8: updated main app
    * uses a twitter and youtube queue to send jobs now

--- src/app.py
+++ src/app.py
@@ -2,12 +2,16 @@ import rq
 import redis
 import time
 from . import twitter
+from . import youtube
 
 def main():
-    q = rq.Queue('twitter', connection=redis.Redis())
+    q1 = rq.Queue('twitter', connection=redis.Redis())
+    q2 = rq.Queue('youtube', connection=redis.Redis())
 
-    job = q.enqueue(twitter.run, {"text": "twitter.com/realDonalTrump"})
+    job1 = q1.enqueue(twitter.run, {"text": "twitter.com/realDonalTrump"})
+    job2 = q2.enqueue(youtube.run, {"text": "https://www.youtube.com/watch?t=3&v=ToyoBTiwZ6c"})
 
     while True:
         time.sleep(1)
-        print(job.result)
+        print("twitter", job1.result)
+        print("youtube", job2.result)
