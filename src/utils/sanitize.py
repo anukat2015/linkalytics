3cb95837da1d82e445a678692b890c49509a2f20
Eric Chu <eytchu@gmail.com>
2015-09-17 14:45:32 -0700

    MEM-8: add sanitize functionality
    This commit updates requirements.txt to include nltk and also
    provides a sanitize function to strip HTML tags from posts and
    remove English stopwords.

--- src/utils/sanitize.py
+++ src/utils/sanitize.py
@@ -1,20 +1,36 @@
 import html.parser
+from nltk.corpus import stopwords
+
+STOP = stopwords.words('english')
+def sanitize(text):
+    """ This sanitizes post text by removing all HTML tags and appending
+        those in <a href="..."></a> to the end of the text.
+
+        It also removes English stopwords.
+    """
+    s = HTMLStripper()
+    s.feed(text)
+    s.close()
+    text = s.get_data()
+
+    return ' '.join(s for s in text.split() if s.lower() not in STOP)
 
 # strip all HTML tags from the text (except <a href="..."></a>)
 # modified from http://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
 class HTMLStripper(html.parser.HTMLParser):
-	def __init__(self):
-		super(HTMLStripper, self).__init__(convert_charrefs=True)
-		self.reset()
-		self.fed = []
+    def __init__(self):
+        super(HTMLStripper, self).__init__(convert_charrefs=True)
+        self.reset()
+        self.fed = []
+        self.hrefs = []
 
-	def handle_starttag(self, tag, attrs):
-		attrs = dict(attrs)
-		if tag == 'a' and 'href' in attrs:
-			self.fed.append(" " + attrs['href'] + " ")
+    def handle_starttag(self, tag, attrs):
+        attrs = dict(attrs)
+        if tag == 'a' and 'href' in attrs:
+            self.hrefs.append(attrs['href'])
 
-	def handle_data(self, d):
-	    self.fed.append(d)
+    def handle_data(self, d):
+        self.fed.append(d)
 
-	def get_data(self):
-	    return ''.join(self.fed)
+    def get_data(self):
+        return ''.join(self.fed) + " :HREFS: " + ' '.join(self.hrefs)
