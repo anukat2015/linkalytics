3323c1774f859f5d770ef6a17d6bdf9b666f7bf3
Eric Chu <eytchu@gmail.com>
2015-09-22 12:10:02 -0700

    MEM-8: fix formatting
    This commit removes tabs from some of our old python files.

--- src/twitter/twitter.py
+++ src/twitter/twitter.py
@@ -15,53 +15,53 @@ twitter_regex = re.compile('twitter\s*-*@*:*;*(\.com\/)?_*\.*\s*([^\s^\/]*)',re.
 
 @retry(on=tweepy.error.TweepError)
 def get_user(twitter_id):
-	return api.get_user(twitter_id)
+    return api.get_user(twitter_id)
 
 @retry(on=tweepy.error.TweepError)
 def get_tweets(twitter_id):
-	return [tweet.text for tweet in api.user_timeline(twitter_id)]
+    return [tweet.text for tweet in api.user_timeline(twitter_id)]
 
 @retry(on=tweepy.error.TweepError)
 def get_friends(user):
-	if user is not None:
-		return [f.screen_name for f in user.friends()]
-	return None
+    if user is not None:
+        return [f.screen_name for f in user.friends()]
+    return None
 
 @retry(on=tweepy.error.TweepError)
 def get_followers(user):
-	if user is not None:
-		return [f.screen_name for f in user.followers()]
-	return None
+    if user is not None:
+        return [f.screen_name for f in user.followers()]
+    return None
 
 def run(node):
-	results = []
-	text = sanitize(node['text'])
-	for identity in re.finditer(twitter_regex, text):
-		twitter_id = identity.group(2).lower()  # username is always the second group in the regex match
-		user = get_user(twitter_id)
-		if user is not None:
-			output_node = {
-				'id'			: twitter_id,
-				'friends'		: get_friends(user),
-				'followers'		: get_followers(user),
-				'background_pic': user.profile_background_image_url_https,
-				'profile_pic'	: user.profile_image_url_https,
-				'description'	: user.description,
-				'name'			: user.name,
-				'profile_url'	: user.entities['url']['urls'][0]['expanded_url'] if 'url' in user.entities else None,
-				'tweets'		: get_tweets(twitter_id)
-			}
-		else:
-			output_node = {
-				'id'			: twitter_id,
-				'friends'		: None,
-				'followers'		: None,
-				'background_pic': None,
-				'profile_pic'	: None,
-				'description'	: None,
-				'name'			: None,
-				'profile_url'	: None,
-				'tweets'		: get_tweets(twitter_id)
-			}
-		results.append(output_node)
-	return {'twitter': uniq_lod(results, 'id')}
+    results = []
+    text = sanitize(node['text'])
+    for identity in re.finditer(twitter_regex, text):
+        twitter_id = identity.group(2).lower()  # username is always the second group in the regex match
+        user = get_user(twitter_id)
+        if user is not None:
+            output_node = {
+                'id'            : twitter_id,
+                'friends'       : get_friends(user),
+                'followers'     : get_followers(user),
+                'background_pic': user.profile_background_image_url_https,
+                'profile_pic'   : user.profile_image_url_https,
+                'description'   : user.description,
+                'name'          : user.name,
+                'profile_url'   : user.entities['url']['urls'][0]['expanded_url'] if 'url' in user.entities else None,
+                'tweets'        : get_tweets(twitter_id)
+            }
+        else:
+            output_node = {
+                'id'            : twitter_id,
+                'friends'       : None,
+                'followers'     : None,
+                'background_pic': None,
+                'profile_pic'   : None,
+                'description'   : None,
+                'name'          : None,
+                'profile_url'   : None,
+                'tweets'        : get_tweets(twitter_id)
+            }
+        results.append(output_node)
+    return {'twitter': uniq_lod(results, 'id')}
--- src/youtube/youtuber.py
+++ src/youtube/youtuber.py
@@ -14,29 +14,29 @@ YOUTUBE_API_VERSION = "v3"
 
 youtube_regex = re.compile('youtube\.com\/embed\/([^\s]*)|youtu\.be\/([^\s]*)|youtube\.com\/watch\?[^\s]*v=([^\s]*)',re.IGNORECASE)
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-	developerKey=YOUTUBE_DEVELOPER_KEY)
+    developerKey=YOUTUBE_DEVELOPER_KEY)
 
 def get_username_from_video(identity):
-	# seed_html=requests.get(identity).content #identity represents embedded Youtube videos
-	# seedsoup=BeautifulSoup(seed_html)
-	# youtubelink=seedsoup.find_all("link")[0].get("href")
-	vid_search = youtube.search().list(
-		q=identity,
-		part="id,snippet",
-		maxResults=1
-		).execute()
+    # seed_html=requests.get(identity).content #identity represents embedded Youtube videos
+    # seedsoup=BeautifulSoup(seed_html)
+    # youtubelink=seedsoup.find_all("link")[0].get("href")
+    vid_search = youtube.search().list(
+        q=identity,
+        part="id,snippet",
+        maxResults=1
+        ).execute()
 
-	user = vid_search.get("items", [])[0]["snippet"]["channelTitle"]
-	return user
+    user = vid_search.get("items", [])[0]["snippet"]["channelTitle"]
+    return user
 
 def run(node):
-	results = []
-	text = sanitize(node['text'])
-	for identity in re.finditer(youtube_regex, text):
-		video_id = next(filter(None,identity.groups()))	# this filters out any empty matches
-		results.append({
-			'video_id': video_id,
-			'username': get_username_from_video(video_id)
-		})
-	uniq_results = list({v['video_id']:v for v in results}.values())
-	return {'youtube': results}
+    results = []
+    text = sanitize(node['text'])
+    for identity in re.finditer(youtube_regex, text):
+        video_id = next(filter(None,identity.groups()))    # this filters out any empty matches
+        results.append({
+            'video_id': video_id,
+            'username': get_username_from_video(video_id)
+        })
+    uniq_results = list({v['video_id']:v for v in results}.values())
+    return {'youtube': results}
