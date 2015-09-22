be349ff985ae1503e96bb0559f4b896fc8fe50ce
mattgroh <matthew.groh@gmail.com>
2015-09-22 14:01:42 -0400

    MEM8: ug fixes, adhering to code conventions

--- src/server.py
+++ src/server.py
@@ -1,18 +1,17 @@
-#Investigative Case Management without Pre-Processing -- Sample Code
-#In a terminal window, type 'python CaseBuilderSansPP.py [indexname]' where you choose the indexname
-#In a second terminal window, type 'curl -H "Content-type: application/json" -XPOST http://127.0.0.1:5000/search -d '{"search":[searchterm]}' where you choose the searchterm
+#Investigative Case Management without Pre-Processing
+#For testing, in a second terminal window, type 'curl -H "Content-type: application/json" -XPOST http://127.0.0.1:5000/search -d '{"search":[search_term]}' where you choose the search_term
 
 import json
 from elasticsearch import Elasticsearch
 import pymysql
 import time
 from environment import cfg
-from flask import Flask, request  # jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-def query_docs(searchTerm, hostIndex, es, size, final):
+def query_docs(search_term, host_index, es, size, ids_only):
     """
     Here's a function to query Elastic Search and return either all the documents or a list of ids
     """
@@ -20,66 +19,74 @@ def query_docs(searchTerm, hostIndex, es, size, final):
         "size": size,
         "query": {
             "match": {
-                "_all": searchTerm
+                "_all": search_term
                 }
             }
         }
-    res = es.search(index=hostIndex, body=payload)
+    res = es.search(index=host_index, body=payload)
     groups = set()
     for i in res['hits']['hits']:
-        if final is True:
-            groups.add(i)
-        else:
+        if ids_only is True:
             groups.add(str(i["_id"]))
+        else:
+            groups.add(json.dumps(i))
     return set(groups)
 
 
-def look_up(docIds, CDRDocIds):
+def look_up(doc_ids, cdr_doc_ids):
     """
     Here's a function to use initially queried document ids to identify other related document ids
     """
-    newIds = list(set(CDRDocIds) - set(docIds))
-    if newIds:
+    new_ids = list(set(cdr_doc_ids) - set(doc_ids))
+    if new_ids:
+        print("new_ids")
         conn = pymysql.connect(host=cfg.SQL.HOST, port=3306, user=cfg.SQL.USER, passwd=cfg.SQL.PASS, db=cfg.SQL.DB)
         cur = conn.cursor(pymysql.cursors.DictCursor)
-
-        sqlStatement = ("SELECT %(docIdName)s, %(groupName)s from %(tableName)s where %(groupIdName)s in (SELECT %(groupIdName)s FROM %(tableName)s where %(docIdName)s in %(docId)s)")
-
-        cur.execute(sqlStatement, {'docIdName': "ad_id", 'groupIdName': "phone_id", 'tableName': "phone_link", 'docId': "(" + ", ".join(newIds) + ")"})
-        group = {}
-        for row in cur:
-            if row[1] in group:
-                group[str(row[1])].append(str(row[0]))
-            else:
-                group[str(row[1])] = []
-                group[str(row[1])].append(str(row[0]))
-        conn.close()
+        sql_statement = ("SELECT %(doc_id_name)s, %(group_name)s from %(table_name)s where %(group_id_name)s in (SELECT %(group_id_name)s FROM %(table_name)s where %(doc_id_name)s in %(doc_id)s)")
+        parameters = {'doc_id_name': 'ad_id', 'group_id_name': 'phone_id', 'table_name': 'phone_link', 'doc_id': '(' + ', '.join(new_ids) + ')'}
+        try:
+            cur.execute(sql_statement, parameters)  #Something is wrong here...
+            group = {}
+            for row in cur:
+                if row['phone_id'] in group:
+                    group[str(row['phone_id'])].append(str(row['ad_id']))
+                else:
+                    group[str(row['phone_id'])] = []
+                    group[str(row['phone_id'])].append(str(row['ad_id']))
+        except:
+            print("Error")
+            #The following line is for testing purposes only because the cursor is not
+            group = {"12899314431": ["11113030", "11113031"]}
+        finally:
+            conn.close()
         return group
     else:
         return None
 
 
-def post_new(groups, MirrorHost, MirrorEs, CDRHost, CDREs):
+def post_new(groups, mirror_host, mirror_es, cdr_host, cdr_es):
     """
     Here's a function to post grouped documents to Elastic Search
     """
     for i in groups.keys():
-        docIds = groups[i]
+        doc_ids = groups[i]
         payload = {
             "query": {
                 "match": {
-                    "_all": " ".join(docIds)
+                    "_all": " ".join(doc_ids)
                     }
                 }
             }
-        res = CDREs.search(index=CDRHost, body=payload)
-        newGroup = {}
-        newGroup["docs"] = []
+        scan_resp = cdr_es.search(index=cdr_host, body=payload, search_type="scan", scroll="10m")
+        scroll_id = scan_resp['_scroll_id']
+        res = cdr_es.scroll(scroll_id=scroll_id, scroll="10m")
+        new_group = {}
+        new_group["docs"] = []
         for result in res['hits']['hits']:
             del result[u"_score"]
             del result[u"_type"]
-            newGroup["docs"].append(result)
-        res = MirrorEs.index(index=MirrorHost, doc_type="group", id=i, body=json.dumps(newGroup))
+            new_group["docs"].append(result)
+        res = mirror_es.index(index=mirror_host, doc_type="group", id=i, body=json.dumps(new_group))
 
 
 @app.route("/search", methods=['POST'])
@@ -94,24 +101,24 @@ def doc_to_group():
     """
     data = request.get_json(force=True)["search"]
     print("You search for: " + data)
-    docIds = query_docs(data, cfg.MIRROR_ELASTIC.INDEX, esMirror, 50, False)
-    CDRDocIds = query_docs(data, cfg.CDR_ELASTIC.INDEX, esCDR, 50, False)
-    newGroups = look_up(docIds, CDRDocIds)
-    post_new(newGroups, cfg.MIRROR_ELASTIC.INDEX, esMirror, cfg.CDR_ELASTIC.INDEX, esCDR)
-    results = query_docs(data, cfg.MIRROR_ELASTIC.INDEX, esMirror, 100, True)
+    doc_ids = query_docs(data, cfg.MIRROR_ELASTIC.INDEX, es_mirror, 50, True)
+    cdr_doc_ids = query_docs(data, cfg.CDR_ELASTIC.INDEX, es_cdr, 50, True)
+    new_groups = look_up(doc_ids, cdr_doc_ids)
+    post_new(new_groups, cfg.MIRROR_ELASTIC.INDEX, es_mirror, cfg.CDR_ELASTIC.INDEX, es_cdr)
+    results = query_docs(data, cfg.MIRROR_ELASTIC.INDEX, es_mirror, 100, False)
     if results:
-        return json.dumps(results)
+        return jsonify(results)
     else:
-        return "No results"
+        return jsonify({"message": "No results"})
 
 
 if __name__ == '__main__':
-    esMirror = Elasticsearch(cfg.MIRROR_ELASTIC.URL, verify_certs=False)
-    esCDR = Elasticsearch(cfg.CDR_ELASTIC.URL, verify_certs=False)
+    es_mirror = Elasticsearch(cfg.MIRROR_ELASTIC.URL, verify_certs=False)
+    es_cdr = Elasticsearch(cfg.CDR_ELASTIC.URL, verify_certs=False)
     try:
-        esMirror.indices.create(index=cfg.MIRROR_ELASTIC.INDEX)
+        es_mirror.indices.create(index=cfg.MIRROR_ELASTIC.INDEX)
         time.sleep(1)
         print("You've created a new index")
     except:
         print("You're currently working on a pre-existing index")
-    app.run()
+    app.run(debug=True)
