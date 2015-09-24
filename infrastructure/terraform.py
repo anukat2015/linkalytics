a862c9f159b23f8d3432d5a088820cdee2ed564f
Eric Chu <eytchu@gmail.com>
2015-09-24 12:47:25 -0700

    MEM-8: update terraform.py
    This commit fixes the default search path for `tfstate` files in terraform.py.

--- infrastructure/terraform.py
+++ infrastructure/terraform.py
@@ -525,8 +525,7 @@ def main():
                         action='store_true',
                         help='with --list, exclude hostvars')
     default_root = os.environ.get('TERRAFORM_STATE_ROOT',
-                                  os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                               '..', '..', )))
+                                  os.path.abspath(os.path.dirname(__file__)))
     parser.add_argument('--root',
                         default=default_root,
                         help='custom root to search for `.tfstate`s in')
