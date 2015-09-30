4b4aa1b70c28362e84d666422738f68ea1115dc7
mattgroh <matthew.groh@gmail.com>
2015-09-30 12:57:23 -0400

    Updating sample environment to incorporate .cfg files

--- src/sample_environment.py
+++ src/sample_environment.py
@@ -13,57 +13,46 @@ print(env.cfg.TWITTER.KEY)
 """
 import os
 import collections
+import pytoml as toml
 
 prod_env = os.getenv('PRODUCTION', False)
 
-# This contains an API key and its secret
-API = collections.namedtuple('API', ('KEY', 'SECRET'))
-
-# This contains information for databases
-Database = collections.namedtuple('Database', ('HOST', 'PORT', 'DB', 'USER', 'PASS'))
+with open('/environment/common.cfg', 'rb') as fin:
+    common = toml.load(fin)
+with open('/environment/develop.cfg', 'rb') as fin:
+    develop = toml.load(fin)
+with open('/environment/production.cfg', 'rb') as fin:
+    production = toml.load(fin)
 
 # This contains configuration environments
 Config = collections.namedtuple('Config', (
-    'TWITTER_ACCESS',       # twitter access key and secret
-    'TWITTER_CONSUMER',     # twitter consumer key and secret
-    'INSTAGRAM',            # instagram access key and secret
+    'TWITTER_',             # twitter creds
+    'INSTAGRAM_CONSUMER',   # instagram creds
     'YOUTUBE',              # youtube developer key
-    'ELS'                   # elasticsearch instance
-    # you can include your own fields here
+    'SQL',                  # sql instance
+    'CDR_ELASTIC',          # elasticsearch instance for common data repository
+    'MIRROR_ELASTIC'       # elasticsearch instance for mirrored local instance
+
+    # you can include your own fields too!
 ))
-
-# This is the twitter access API key
-__twitter_access = API(KEY="k3y", SECRET="t0ps3cr3t")
-
-# This is the twitter consumer API key
-__twitter_consumer = API(KEY="k3y", SECRET="t0ps3cr3t")
-
-# This is the common instagram API key
-__instagram = API(KEY="k3y", SECRET="t0ps3cr3t")
-
-# This is the common youtube API key, the SECRET is None since it's
-# just a developer API key
-__youtube = API(KEY="k3y", SECRET=None)
-
-# This is the MEMEX ES instance, DB is the index
-__memex_els = Database(HOST='localhost', PORT=9200, DB="my_index", USER="root", PASS="s3cr3t")
-
 
 if prod_env:
     # Any production environments go here....
     cfg = Config(
-        TWITTER_ACCESS      = __twitter_access,
-        TWITTER_CONSUMER    = __twitter_consumer,
-        INSTAGRAM           = __instagram,
-        YOUTUBE             = __youtube,
-        ELS                 = __memex_els
+        TWITTER             = common["twitter"]
+        INSTAGRAM           = common["instagram"],
+        YOUTUBE             = common["youtube"],
+        SQL                 = production["sql"],
+        CDR_ELASTIC         = common["cdr_elastic_search"],
+        MIRROR_ELASTIC      = production["mirror_elastic_search"]
     )
 else:
     # Any other environments go here....
     cfg = Config(
-        TWITTER_ACCESS      = __twitter_access,
-        TWITTER_CONSUMER    = __twitter_consumer,
-        INSTAGRAM           = __instagram,
-        YOUTUBE             = __youtube,
-        ELS                 = __memex_els
+        TWITTER             = common["twitter"]
+        INSTAGRAM           = common["instagram"],
+        YOUTUBE             = common["youtube"],
+        SQL                 = develop["sql"],
+        CDR_ELASTIC         = common["cdr_elastic_search"],
+        MIRROR_ELASTIC      = develop["mirror_elastic_search"]
     )
