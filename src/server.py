ac611a0680b2195011954542abbb8f9f5dbe05de
Sang Han <jjangsangy@gmail.com>
2015-09-30 19:44:44 -0700

    changed the formatting for better readability

--- src/server.py
+++ src/server.py
@@ -1,5 +1,15 @@
-#Investigative Case Management without Pre-Processing
-#For testing, in a second terminal window, type 'curl -H "Content-type: application/json" -XPOST http://127.0.0.1:5000/search -d '{"search":[search_term]}' where you choose the search_term
+"""
+Investigative Case Management without Pre-Processing
+====================================================
+
+For testing, in a second terminal window, type
+
+    curl -H "Content-type: application/json" \
+         -d '{"search":[search_term]}' \
+        -XPOST http://127.0.0.1:5000/search
+
+where you choose the search_term.
+"""
 
 import json
 from elasticsearch import Elasticsearch
@@ -20,9 +30,9 @@ def query_docs(search_term, host_index, es, size, ids_only, cdr):
         "query": {
             "match_phrase": {
                 "_all": search_term
-                }
             }
         }
+    }
     res = es.search(index=host_index, body=payload)
     groups = set()
     for i in res['hits']['hits']:
@@ -40,16 +50,31 @@ def query_docs(search_term, host_index, es, size, ids_only, cdr):
 
 def look_up(doc_ids, cdr_doc_ids):
     """
-    Here's a function to use initially queried document ids to identify other related document ids
+    To use initially queried document ids to identify other related document ids
     """
     new_ids = list(set(cdr_doc_ids) - set(doc_ids))
+
     if new_ids:
-        conn = pymysql.connect(host=cfg["sql"]["host"], port=3306, user=cfg["sql"]["user"], passwd=cfg["sql"]["password"], db=cfg["sql"]["database"])
+
+        conn = pymysql.connect(host=cfg["sql"]["host"],
+                               port=3306,
+                               user=cfg["sql"]["user"],
+                               passwd=cfg["sql"]["password"],
+                               db=cfg["sql"]["database"]
+        )
         cur = conn.cursor(pymysql.cursors.DictCursor)
+
+        #TODO: Increase speed by adding join
         sql_statement = """SELECT `phone_id`, `ad_id`
-            FROM `phone_link` WHERE `phone_id` IN (
-            SELECT `phone_id` FROM `phone_link` WHERE `ad_id` in %s )"""
-            #ToDo: Increase speed by adding join
+                           FROM   `phone_link`
+                           WHERE  `phone_id`
+                           IN (
+                               SELECT `phone_id`
+                               FROM  `phone_link`
+                               WHERE `ad_id`
+                               IN %s
+                           )
+                        """
         try:
             cur.execute(sql_statement, (list(map(int, new_ids)),))
             group = {}
@@ -60,13 +85,18 @@ def look_up(doc_ids, cdr_doc_ids):
                 else:
                     group[str(row['phone_id'])] = []
                     group[str(row['phone_id'])].append(str(row['ad_id']))
+
             print("Your search identified the following new groups:")
             print(group)
+
         except:
             print("Error")
+
         finally:
             conn.close()
+
         return group
+
     else:
         return None
 
@@ -81,18 +111,20 @@ def post_new(groups, mirror_host, mirror_es, cdr_host, cdr_es):
             "query": {
                 "match": {
                     "_all": " ".join(doc_ids)
-                    }
                 }
             }
+        }
         scan_resp = cdr_es.search(index=cdr_host, body=payload, search_type="scan", scroll="10m")
         scroll_id = scan_resp['_scroll_id']
         res = cdr_es.scroll(scroll_id=scroll_id, scroll="10m")
         new_group = {}
         new_group["docs"] = []
+
         for result in res['hits']['hits']:
             del result[u"_score"]
             del result[u"_type"]
             new_group["docs"].append(result)
+
         #Add Instagram, Twitter, and Youtube add-ons here
         res = mirror_es.index(index=mirror_host, doc_type="group", id=i, body=json.dumps(new_group))
         print("Posted successfully")
@@ -101,7 +133,9 @@ def post_new(groups, mirror_host, mirror_es, cdr_host, cdr_es):
 @app.route("/search", methods=['POST'])
 def doc_to_group():
     """
-    Here's a server that takes a search term as an input and provides a list of grouped documents as an output
+    Here's a server that takes a search term as an input
+    and provides a list of grouped documents as an output
+
     Step 1 -- Query Mirror Elastic
     Step 2 -- Query CDR Elastic
     Step 3 -- Check Lookup Table
@@ -113,18 +147,24 @@ def doc_to_group():
     search_term = request.get_json(force=True)["search"]
     # print("You searched for: " + search_term)
     doc_ids = query_docs(search_term, cfg["mirror_elastic_search"]["index"], es_mirror, 50, True, False)
+
     # print("# of Results from Mirror: " + str(len(doc_ids)))
     cdr_doc_ids = query_docs(search_term, cfg["cdr_elastic_search"]["index"], es_cdr, 50, True, True)
+
     # print("# of Results from CDR: " + str(len(cdr_doc_ids)))
     new_groups = look_up(doc_ids, cdr_doc_ids)
+
     if new_groups:
         print("# of New Groups: " + str(len(new_groups)))
         post_new(new_groups, cfg["mirror_elastic_search"]["index"], es_mirror, cfg["cdr_elastic_search"]["index"], es_cdr)
         time.sleep(2)
+
     else:
         print("No new groups")
+
     results = query_docs(search_term, cfg["mirror_elastic_search"]["index"], es_mirror, 100, False, True)
     #print("Results: " + str(len(results)))
+
     if results:
         return jsonify(results=results)
     else:
@@ -133,7 +173,9 @@ def doc_to_group():
 
 def test_doc_to_group(search):
     """
-    Here's a server that takes a search term as an input and provides a list of grouped documents as an output
+    Here's a server that takes a search term as an input
+    Provides a list of grouped documents as an output
+
     Step 1 -- Query Mirror Elastic
     Step 2 -- Query CDR Elastic
     Step 3 -- Check Lookup Table
@@ -141,20 +183,27 @@ def test_doc_to_group(search):
     Step 5 -- Query newly updated Mirror Elastic
     """
     search_term = search["text"]
+
     # print("You searched for: " + search_term)
     doc_ids = query_docs(search_term, mirror_elastic_index, es_mirror, 50, True, False)
+
     # print("# of Results from Mirror: " + str(len(doc_ids)))
     cdr_doc_ids = query_docs(search_term, cdr_elastic_index, es_cdr, 50, True, True)
+
     # print("# of Results from CDR: " + str(len(cdr_doc_ids)))
     new_groups = look_up(doc_ids, cdr_doc_ids)
+
     if new_groups:
         print("# of New Groups: " + str(len(new_groups)))
         post_new(new_groups, mirror_elastic_index, es_mirror, cdr_elastic_index, es_cdr)
         time.sleep(1.2)
+
     else:
         print("No new groups")
+
     results = query_docs(search_term, mirror_elastic_index, es_mirror, 100, False, True)
     #print("Results: " + str(len(results)))
+
     if results:
         return jsonify(results=results)
     else:
@@ -162,14 +211,18 @@ def test_doc_to_group(search):
 
 
 if __name__ == '__main__':
+
     es_mirror = Elasticsearch(cfg["mirror_elastic_search"]["hosts"], verify_certs=False)
     mirror_elastic_index = cfg["mirror_elastic_search"]["index"]
     es_cdr = Elasticsearch(cfg["cdr_elastic_search"]["hosts"], verify_certs=False)
     cdr_elastic_index = cfg["cdr_elastic_search"]["index"]
+
     try:
         es_mirror.indices.create(index=cfg["mirror_elastic_search"]["index"])
         time.sleep(1)
         print("You've created a new index")
+
     except:
         print("You're currently working on a pre-existing index")
+
     app.run(debug=True)
