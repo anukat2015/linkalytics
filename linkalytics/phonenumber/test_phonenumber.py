b0cb859e13815a84ce563c790982cac062bff531
Sang Han <jjangsangy@gmail.com>
2015-11-15 06:31:20 -0800

    added more unit tests

---
+++ linkalytics/phonenumber/test_phonenumber.py
@@ -0,0 +1,17 @@
+from .. utils import with_test
+
+from . import phonenumber
+
+@with_test(phonenumber.run, 'phonenumbers')
+def data():
+    yield (
+        { 'text': '1800295 408-291-2521' }, [
+            {
+                'carrier': '',
+                'location': 'California',
+                'number': '+14082912521',
+                'time_zone': ('America/Los_Angeles',)
+            }
+        ]
+    )
+
--- linkalytics/twitter/test_twitter.py
+++ linkalytics/twitter/test_twitter.py
@@ -1,9 +1,16 @@
-import nose
 from .. utils import with_test
+
 from . twitter import run
 
 @with_test(run, 'twitter')
 def data():
-    yield ({"text": "twitter.com/realDonaldTrump"}, [
-            {"id": "realdonaldtrump", "name": "Donald J. Trump"}
-        ])
+    yield (
+        {
+            "text": "twitter.com/realDonaldTrump",
+        }, [
+            {
+                "id": "realdonaldtrump",
+                "name": "Donald J. Trump",
+            }
+        ]
+    )
