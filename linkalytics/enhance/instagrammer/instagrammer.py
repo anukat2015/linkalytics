610915fc2d0d98f4dc3d20bae318a47bc893c40f
Sang Han <jjangsangy@gmail.com>
2015-11-18 15:30:35 -0800

    instagram complete

--- linkalytics/enhance/instagrammer/instagrammer.py
+++ linkalytics/enhance/instagrammer/instagrammer.py
@@ -1,7 +1,5 @@
-import instagram
 import logging
 import re
-import requests
 
 from ... environment import cfg
 from ... utils import sanitize, uniq_lod, push_url
@@ -9,12 +7,18 @@ from ... utils import sanitize, uniq_lod, push_url
 logging.basicConfig()
 log = logging.getLogger("linkalytics.instagram")
 
-api = instagram.client.InstagramAPI(
-	client_id	= cfg['instagram']['client_id'],
-	client_secret	= cfg['instagram']['client_secret'],
-	access_token	= cfg['instagram']['access_token'],
-)
 instagram_regex = re.compile('instagram\s*-?@?:?;?(\.com\/)?_?\.?\s*([^\s^\/]*)',re.IGNORECASE)
+
+@push_url('https://api.instagram.com/v1/')
+def get_instagram(endpoint, **kwargs):
+    query = {
+        'url': endpoint,
+        'params': {
+            'access_token': cfg['instagram']['access_token']
+        }
+    }
+    query['params'].update(kwargs)
+    return query
 
 def get_user_id(username):
     return get_instagram('users/search', q=username)['data'][0]['id']
@@ -23,18 +27,17 @@ def get_user(uid):
     return get_instagram('users/{uid}'.format(uid=uid))['data']
 
 def get_recent_posts(uid):
-    return get_instagram('users/{uid}/media/recent'.format(uid=uid))['data']
-
-@push_url('https://api.instagram.com/v1/')
-def get_instagram(endpoint, **kwargs):
-    query = {
-		'url': endpoint,
-		'params': {
-			'access_token': cfg['instagram']['access_token']
-		}
-	}
-    query['params'].update(kwargs)
-    return query
+    posts  = get_instagram('users/{uid}/media/recent'.format(uid=uid))['data']
+    output = {
+        post['id']: {
+            'link'       : post['link'],
+            'likes'      : post['likes']['count'],
+            'tags'       : post['tags'],
+            'caption'    : post['caption']['text'],
+            'attribution': post['attribution'],
+        }  for post in posts
+    }
+    return output
 
 def run(node):
     results = []
