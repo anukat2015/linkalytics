b6ba7a943c72a3f1e0ba7a6be10c681fbf644f63
Sang Han <jjangsangy@gmail.com>
2015-11-18 17:04:29 -0800

    added tika server

---
+++ infrastructure/tika.tf
@@ -0,0 +1,23 @@
+resource "google_compute_instance" "tika-server" {
+  name          = "tika-${count.index}"
+  description   = "The VM hosting a tika server"
+  machine_type  = "n1-standard-1"
+  zone          = "us-central1-b"
+  tags          = ["memex", "linkalytics", "tika", "server"]
+
+  disk {
+      image = "debian-8-jessie-v20150915"
+      size  = 20
+  }
+
+  metadata {
+    sshKeys = "ansible:${file("keys/gce.pub")}"
+  }
+
+  network_interface {
+      network = "default"
+      access_config {}
+  }
+
+  count = 1
+}
