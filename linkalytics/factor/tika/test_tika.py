afc0320085533b60c243c556e239ee26f6bb3554
Sang Han <jjangsangy@gmail.com>
2015-11-23 14:51:03 -0800

    initial tika testing: go travis

--- linkalytics/factor/tika/test_tika.py
+++ linkalytics/factor/tika/test_tika.py
@@ -1,8 +1,20 @@
+import redis
+
 from unittest import TestCase
 
-from . import extract
+from . import extract, tika
+
+from ... environment import cfg
+
+redis_instance = redis.Redis(host=cfg['redis']['host'])
 
 class TestExtract(TestCase):
 
     def test_common_crawl(self):
         self.assertTrue(any(extract.filter_docs(extract.common_crawl('http://www.toysrus.com'))))
+
+
+class TestTika(TestCase):
+
+    def test_redis_load(self):
+        self.assertTrue(any(tika.redis_load('http://toysrus.com', redis_instance)))
