97862f931487c9d2fc9f8a36d99b727925db88be
Sang Han <jjangsangy@gmail.com>
2015-12-02 16:12:44 -0800

    added more tests

--- linkalytics/api/test_api.py
+++ linkalytics/api/test_api.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from base64   import b64encode
 from flask    import current_app
 
 from .. import create_app
@@ -11,9 +12,26 @@ class APITest(TestCase):
         self.app = create_app(cfg)
         self.app_context = self.app.app_context()
         self.app_context.push()
+        self.client = self.app.test_client(use_cookies=True)
 
     def tearDown(self):
         self.app_context.pop()
 
+    def get_api_headers(self, username, password):
+        auth = b64encode('{username}:{password}'.format(username=username, password=password).encode('utf-8')).decode('utf-8')
+        return {
+            'Authorization': 'Basic {auth}'.format(auth=auth)
+
+        }
+
     def test_app_exists(self):
         self.assertFalse(current_app is None)
+
+    def test_page_not_found(self):
+        response = self.client.post(
+            '/wrong_path',
+            content_type='application/json',
+            headers=self.get_api_headers(cfg['api']['username'], cfg['api']['password'])
+        )
+        self.assertTrue(response.status_code == 404)
+
