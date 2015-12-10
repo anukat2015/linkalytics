604a104fc585f251f69dc7c41c7b2f17f7590804
mattgroh <matthew.groh@gmail.com>
2015-12-10 14:39:09 -0500

    Cleaning up recursor output -- remove empty responses

--- linkalytics/factor/constructor/recursor.py
+++ linkalytics/factor/constructor/recursor.py
@@ -1,10 +1,8 @@
 from ... environment import cfg
 from ... run_cli import Arguments
 
-from .. lsh import lsh
-
 from . elasticfactor import ElasticFactor
-import json
+
 
 def run(node):
     _id, factor_fields, factor_values, factors = node.get('_id', '1449687484'), node.get('factor_fields', ['phone', 'phone']), node.get('factor_values', ['5023030050', '5023691601']), node.get('factors', ['phone', 'email', 'text', 'title'])
@@ -20,7 +18,7 @@ def run(node):
                 if isinstance(v2, dict):
                     for k3, v3 in v2.items():  # v3 is factor_value
                         if v3:
-                            for k4 in v3.keys(): # k4 is factor_value
+                            for k4 in v3.keys():  # k4 is factor_value
                                 state_identifiers.append(k4)
     for i in range(0, len(factor_fields)):
         additions = constructor.reverse_lookup(factor_fields[i], factor_values[i])
@@ -34,6 +32,8 @@ def run(node):
                         if v2:
                             if k3 not in state_identifiers:
                                 state[factor_fields[i] + "_" + factor_values[i]][k1][k2] = v2
+                if state[factor_fields[i] + "_" + factor_values[i]][k1] == {}:
+                    state[factor_fields[i] + "_" + factor_values[i]].pop(k1)
         if state[factor_fields[i] + "_" + factor_values[i]] == {}:
             state.pop(factor_fields[i] + "_" + factor_values[i])
     return state
