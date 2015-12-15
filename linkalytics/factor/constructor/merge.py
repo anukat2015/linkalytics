e5f3f2dbdec1f17d5ab424461fd5835c4f11a821
mattgroh <matthew.groh@gmail.com>
2015-12-15 15:58:14 -0500

    Update merge code to work with method from ElasticFactor.py

--- linkalytics/factor/constructor/merge.py
+++ linkalytics/factor/constructor/merge.py
@@ -1,12 +1,14 @@
 from . elasticfactor import ElasticFactor
 
 from ... environment import cfg
+from elasticsearch import Elasticsearch
+
 
 def run(node):
-    ad_id, factors = node.get('id', '63166071'), node.get('factors', ['phone', 'email', 'text', 'title'])
+    id_a, id_b 	= node.get('id_a', '63166071_1'), node.get('id_b', '63166071_2')
+    es 			= Elasticsearch()
+    data_a 		= es.get(index="factor_state2016", doc_type='factor_network', id=id_a)
+    data_b 		= es.get(index="factor_state2016", doc_type='factor_network', id=id_b)
     constructor = ElasticFactor(cfg["cdr_elastic_search"]["hosts"] + cfg["cdr_elastic_search"]["index"])
-    merged      = constructor.reduce(ad_id, *factors)
-
-    return {
-        ad_id: list(merged)
-    }
+    merged 		= constructor.merge(data_a["_source"], data_b["_source"])
+    return merged
