cfe76d4e1552bac5b416d6186d2bf79c8f0f931f
Sang Han <jjangsangy@gmail.com>
2015-12-17 18:05:47 -0800

    bug fix with code for factor network

--- linkalytics/factor/network/network.py
+++ linkalytics/factor/network/network.py
@@ -1,5 +1,8 @@
+import itertools
+import collections
+
 from elasticsearch import Elasticsearch
-from functools import total_ordering, reduce
+from flask import jsonify
 
 import networkx as nx
 
@@ -45,40 +48,40 @@ class propertycache:
     def cachevalue(self, obj, value):
         setattr(obj, self.name, value)
 
-@total_ordering
-class FactorNode:
+class Node:
     """
-    Concrete implementations should be easily
-    hashable, recursive data structures.
+    Custom parametric data type.
 
-    :param url: str
-        Fully qualified url to an elasticsearch instance
+    Has more consistent hashing behavior than integers
+    and strings when used as keys to dictionaries.
+
+    Example
+    -------
+    >>> hash(Node(500)) == hash(Node('500'))
+    True
+
+    >>> hash(500) == hash('500')
+    False
     """
     def __init__(self, _id):
-        self._id = str(_id)
+        self._id = _id
+
     def __repr__(self):
-        return '<{clsname}({id})>'.format(
+        return '<V|{id}|>'.format(
             clsname=self.__class__.__name__,
-            id=self.id
-        )
-    @property
-    def id(self):
-        return self._id
-    @id.setter
-    def id(self, value):
-        try:
-            self._id = int(value)
-        except ValueError as error:
-            raise error
+            id=self.id)
+
     def __hash__(self):
-        return int(self.id)
+        return hash(self.id)
+
     def __eq__(self, other):
         return self.id == other.id
-    def __lt__(self, other):
-        return self.id < other.id
 
+    @property
+    def id(self):
+        return str(self._id)
 
-class Messenger(metaclass=Singleton):
+class Messenger(metaclass=MetaConfigInjection):
 
     def __init__(self, size=500):
         """
@@ -95,6 +98,7 @@ class Messenger(metaclass=Singleton):
             url='<Elasticsearch URL>',
             size=self.size,
         )
+
     @memoize
     def available(self, ad_id):
         """
@@ -170,36 +174,23 @@ class Messenger(metaclass=Singleton):
         return [
             hit['_id'] for hit in results['hits']['hits']
         ]
+
     def suggest(self, ad_id, field):
         """
         The suggest function suggests other ad_ids that share this
         field with the input ad_id.
         """
-        suggestions = []
+        suggestions = {}
         field_values = self.lookup(ad_id, field)
 
         for value in field_values:
             ads = set(self.reverse_lookup(field, value))
 
             # To prevent cycles
-            if ad_id in ads:
-                ads.remove(ad_id)
-
-            for ad in ads:
-                suggestions.append(ad)
+            ads.discard(ad_id)
+            suggestions[value] = list(ads)
 
         return suggestions
-
-class EdgeStruct:
-    """
-    Data structure applied to edges in a factor network.
-    """
-    def __init__(self, weight=1, **kwargs):
-        self.__dict__ = kwargs
-        self.weight   = weight
-
-    def __repr__(self):
-        return str(self.__dict__)
 
 class FactorNetwork:
     """
@@ -208,33 +199,76 @@ class FactorNetwork:
     Manager class for initializing and
     handling state in a factor network
     """
-    def __init__(self, factor=FactorNode):
+    def __init__(self, Messenger=Messenger, **kwargs):
         """
-        :NOTE: must be hashable
-        :param factor: Node
-            Node Data Primitive
-
-        :rtype:
-            <FactorNetwork>
+        :param Messenger:
+            A class constructor following the suggestion
+            interface
+        :param kwargs:
+            Keyword arguments fed into constructor
+            to initialize local network object
         """
         self.messenger = Messenger()
-        self.factor    = factor
-        self.G         = nx.DiGraph()
-
+        self.G         = nx.Graph(**kwargs)
 
     def __repr__(self):
-        return '{clsname}()'.format(
-                clsname=self.__class__.__name__
+        nodes  = nx.number_of_nodes(self.G)
+        edges  = nx.number_of_edges(self.G)
+        return '{nm}(nodes={nodes}, edges={edges})'.format(
+            nm=self.__class__.__name__,
+            nodes=nodes,
+            edges=edges,
         )
 
+    def get_graph(self, node, factor, **kwargs):
+        """
+        Create the networkx graph representation
+
+        :param node: str
+            Document ID of the root node
+        :param factor: str
+            A type of factor to query
+        :param kwargs:
+            Keyword arguments fed into constructor
+            to initialize local network object
+        """
+        G, node = nx.Graph(**kwargs), str(node)
+        G.add_node(node)
+
+        message = self.messenger.suggest(node, factor)
+
+        for value, keys in message.items():
+            edgelist = itertools.zip_longest([node], keys, fillvalue=node)
+            metadata = {'value': value, 'factor': factor}
+            G.add_edges_from(edgelist, **metadata)
+
+        return G
+
     def register_node(self, node, factor):
+        node = str(node)
+        self.G = nx.compose(self.G, self.get_graph(node, factor))
+
+    def to_dict(self):
         """
-        Takes in a node and it's suggestions and builds
-        out the directed edges.
+        Serialize graph edges back into JSON
         """
-        self.G.add_node(node)
-        suggestion = list(map(self.factor, self.messenger.suggest(node.id, factor)))
-        for edge in suggestion:
-            self.G.add_edge(node, edge)
+        d = collections.defaultdict(list)
+        for leaf, node in nx.edges(self.G):
+            d[node].append(leaf)
+
+        return dict(d)
+
+    def show(self):
+        nx.draw_networkx(self.G,
+                         pos=nx.layout.fruchterman_reingold_layout(self.G),
+                         with_labels=False,
+                         node_size=100,
+        )
 
 
+def run(node):
+    _id, factor = node.get('id'), node.get('factor')
+    network = FactorNetwork()
+    network.register_node(_id, factor)
+
+    return network.to_dict()
