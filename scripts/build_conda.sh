377cd2220e90e8a4f5b092ef35e9b4d9ebdd743a
Sang Han <jjangsangy@gmail.com>
2015-12-22 19:51:10 -0800

    update

--- scripts/build_conda.sh
+++ scripts/build_conda.sh
@@ -1,5 +1,5 @@
 
-if ! [ -d ${HOME}/miniconda ]; then
+if ! [ -f ${HOME}/miniconda/bin/python ]; then
     wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
     bash miniconda.sh -b -p ${HOME}/miniconda
     conda update --yes conda
