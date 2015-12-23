6f3378feda5dda7950864a768f4f4ec6d0d037e4
Sang Han <jjangsangy@gmail.com>
2015-12-22 19:47:16 -0800

    conda cache

--- scripts/build_conda.sh
+++ scripts/build_conda.sh
@@ -1,3 +1,6 @@
-wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p ${HOME}/miniconda
-conda update --yes conda
+
+if ! [ -d ${HOME}/miniconda ]; then
+    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+    bash miniconda.sh -b -p ${HOME}/miniconda
+    conda update --yes conda
+fi
